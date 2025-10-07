dca112bebdbd121bb2545c47304ccefa74dd83d1
gavinleroy <gavinleroy6@gmail.com>
2025-10-07 16:54:35 -0400

    update telem

--- telemetry/vite.config.ts
+++ telemetry/vite.config.ts
@@ -1,18 +1,27 @@
+import fs from "node:fs";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
+let manifest = JSON.parse(fs.readFileSync("package.json", "utf-8"));
 export default defineConfig(({ mode }) => ({
-  base: "./",
   build: {
-    target: "ES2022",
     lib: {
-      name: "Telemetry",
-      entry: resolve(__dirname, "src/lib.ts"),
+      entry: resolve(__dirname, "src/main.ts"),
+      name: "Telemetry2",
       formats: ["iife"]
+    },
+    rollupOptions: {
+      external: Object.keys(manifest.dependencies || {})
     }
   },
   define: {
-    TELEMETRY_URL: JSON.stringify("http://oxcaml-tutorial.gavinleroy.com/log"),
+    TELEMETRY_URL: JSON.stringify("https://oxcaml-tutorial.gavinleroy.com/log"),
     "process.env.NODE_ENV": JSON.stringify(mode)
+  },
+  test: {
+    environment: "jsdom",
+    deps: {
+      inline: [/^(?!.*vitest).*$/]
+    }
   }
 }));
