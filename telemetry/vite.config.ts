50a07964b1e55137c20a0b80aa7726749af7eeef
gavinleroy <gavinleroy6@gmail.com>
2026-01-11 19:50:19 +0100

    commit hash

--- telemetry/vite.config.ts
+++ telemetry/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => ({
   define: {
     TELEMETRY_URL: JSON.stringify("https://oxcaml-tutorial.gavinleroy.com"),
     BRANCH: JSON.stringify("main"),
-    COMMIT_HASH: JSON.stringify("d4c2bc57dad4a44d08e66a99a0b8a2ed7aa3137e"),
+    COMMIT_HASH: JSON.stringify("8d2c8953f2124211d85f17f2219e5a104a9e2a7d"),
     "process.env.NODE_ENV": JSON.stringify(mode)
   },
   test: {
