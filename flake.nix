fdccb31a0ddaa29582c831e022a34fd7831e3ac0
gavinleroy <gavinleroy6@gmail.com>
2026-01-14 17:50:39 +0100

    analysis

--- flake.nix
+++ flake.nix
@@ -81,6 +81,46 @@
           '';
         };
 
+        juliaEnv = pkgs.julia-bin.withPackages [
+          "Pluto"
+          "PlutoUI"
+          "JSON3"
+          "DataFrames"
+          "Dates"
+          "Statistics"
+          "StatsPlots"
+        ];
+
+        analysis = pkgs.stdenv.mkDerivation {
+          name = "analysis";
+          src = pkgs.lib.cleanSource ./analysis;
+          nativeBuildInputs = [ juliaEnv ];
+
+          buildPhase = ''
+            export JULIA_DEPOT_PATH=$(mktemp -d)
+            export JULIA_PKG_OFFLINE=true
+            cp -rL ${juliaEnv}/share/julia/site/* $JULIA_DEPOT_PATH/ 2>/dev/null || true
+
+            julia -e '
+            import Pluto; 
+            session = Pluto.ServerSession();
+            session.options.server.disable_writing_notebook_files = true;
+            session.options.evaluation.workspace_use_distributed = false;
+            nb = Pluto.SessionActions.open(session, "notebook.jl"; run_async=false);
+            html_contents = Pluto.generate_html(nb);
+            write("index.html", html_contents);
+            '
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            mkdir -p $out
+            cp index.html $out/
+            cp -R responses $out/
+            cp -LR assets $out/
+          '';
+        };
+
         sbclServer = pkgs.sbcl.withPackages (
           ps: with ps; [
             woo
@@ -122,10 +162,29 @@
             --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath buildInputs}"
           '';
         };
+
+        website = pkgs.stdenv.mkDerivation {
+          pname = "website";
+          version = "0.1.0";
+          phases = [ "installPhase" ];
+
+          installPhase = ''
+            mkdir -p $out
+            mkdir -p $out/analysis
+            cp -r ${activity-book}/* $out/
+            cp -r ${analysis}/* $out/analysis/
+          '';
+        };
       in
       {
         packages = {
-          inherit telemetry-pkg activity-book oxserver;
+          inherit
+            telemetry-pkg
+            activity-book
+            analysis
+            website
+            oxserver
+            ;
           default = activity-book;
         };
 
@@ -140,7 +199,7 @@
               mdbookqz
               mdbook
 
-              julia-bin
+              juliaEnv
 
               sbclServer
               libev
